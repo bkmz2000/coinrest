08aa2221f9e77f623cb9fdea1606ba9b1942f1fe
AGolicyn <win.rus44@gmail.com>
2024-02-28 22:48:44 +0300

    More db connections

--- app/backend/app/app/src/db/connection.py
+++ app/backend/app/app/src/db/connection.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 SQLALCHEMY_DATABASE_URL = os.environ.get("DB_URL", "postgresql+psycopg://exchange:df456Sdb34@0.0.0.0:6543/market")
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, future=True, pool_size=100, max_overflow=50)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, future=True, pool_size=200, max_overflow=50)
 Base = declarative_base()
 
 AsyncSessionFactory = async_sessionmaker(engine, expire_on_commit=False)

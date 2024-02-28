b8d6efb739141803e5b48d652be22da2ab4b4c8f
AGolicyn <win.rus44@gmail.com>
2024-02-29 01:27:46 +0300

    fix

--- app/backend/app/app/src/tasks/last_price.py
+++ app/backend/app/app/src/tasks/last_price.py
@@ -13,7 +13,7 @@ async def main():
     await asyncio.gather(*[get_price_and_volume(ex=ex) for ex in exchanges])
 
 
-# @sleeping
+@sleeping
 async def get_price_and_volume(ex: str):
     """
         Get last price and volume for exchange tickers and store them to db

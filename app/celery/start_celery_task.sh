b6876538e473bde6d5388db9f46761eccf30f5ca
AGolicyn <win.rus44@gmail.com>
2024-05-07 13:05:18 +0300

    feat: exhanges monitor moved from celery to a separate container. ccxt lib updated

--- app/celery/start_celery_task.sh
+++ app/celery/start_celery_task.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo 'Start last price task and other periodic' && celery -A src.worker call src.worker.last_tickers_task && celery -A src.worker worker --loglevel=INFO
+celery -A src.worker worker --loglevel=INFO
+# echo 'Start last price task and other periodic' && celery -A src.worker call src.worker.last_tickers_task && celery -A src.worker worker --loglevel=INFO

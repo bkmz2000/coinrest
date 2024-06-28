c69635d8dcabcc3dd4ec4c4b9442b16c792655cd
AGolicyn <win.rus44@gmail.com>
2024-06-28 13:38:42 +0300

    fix: exchange charts

--- app/backend/app/app/src/api/rest/charts.py
+++ app/backend/app/app/src/api/rest/charts.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 from typing import Literal
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -36,7 +37,7 @@ CHART = {
         "step": 86400,
     },
     "1Y": {
-        "limit": 360,
+        "limit": 365,
         "type": "daily",
         "step": 86400,
     }
@@ -52,21 +53,27 @@ async def get_charts(exchange_name: str,
     """
     chart_params = CHART.get(period)
     historical = HistoricalCRUD()
+
+    now = int(time.time())
+    diff = now % chart_params["step"]
+    right = now - diff
+    left = right - (chart_params["limit"] * chart_params["step"])
+    chart_params["start"] = left
+
     charts = await historical.get_charts(session=session, chart_params=chart_params, exchange_name=exchange_name)
 
     if currency != "USD":
         currency_rate = await get_fiat_currency_rate(session=session, currency=currency)
     else:
         currency_rate = 1
-
+    print(charts)
+    print(right, left)
     if not charts:
         return schema.ExchangeChartResponse(
             exchange_id=exchange_name,
             period=period,
             data=[]
         )
-    right = max(charts.keys())
-    left = right - (chart_params["limit"] * chart_params["step"])
     step = chart_params["step"]
     chart_entries = []
 
